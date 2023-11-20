dbf127cecdbcb276c84e0e095b3ce0deb6c71234
issa2580 <issadiop2580@gmail.com>
2023-11-20 05:35:19 +0000

    add article details

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,11 @@
 import Logo from '../assets/Logo.svg'
 import Media from '../assets/media.png'
+import Post1 from '../assets/posts/post1.jpg'
 
 export const images = {
     Logo,
-    Media
+    Media,
+    Post1,
 };
 
 // export default images;
